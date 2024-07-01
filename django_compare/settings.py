da12478cac5e6925e7a913806b0e37dedd20f227
Alexey Kurylev <chepe4pi@gmail.com>
2024-07-01 17:33:51 -0300

    Django VS FastAPI - stage 3

--- django_compare/settings.py
+++ django_compare/settings.py
@@ -130,3 +130,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#         },
+#     },
+# }
