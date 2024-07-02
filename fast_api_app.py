7b8d97ebe364207ca9721c609f781e615044896c
Alexey Kurylev <chepe4pi@gmail.com>
2024-07-02 00:09:43 -0300

    Django VS FastAPI - stage 5

--- fast_api_app.py
+++ fast_api_app.py
@@ -102,4 +102,4 @@ async def get_order(order_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=4)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
