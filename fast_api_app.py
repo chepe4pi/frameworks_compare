77b8a6894a1eacfdafbb311315f3feb7c3fce314
Alexey Kurylev <chepe4pi@gmail.com>
2024-07-02 22:46:28 -0300

    Django VS FastAPI - stage 5 - v2 gather

--- fast_api_app.py
+++ fast_api_app.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import databases
 from typing import List
 from fastapi import FastAPI, HTTPException
@@ -57,21 +59,6 @@ async def get_order(order_id: int):
             o.id = :order_id
         LIMIT 21;
     """
-    order_row = await database.fetch_one(query=order_query, values={"order_id": order_id})
-
-    if not order_row:
-        raise HTTPException(status_code=404, detail="Order not found")
-
-    order_instance = {
-        'id': order_row['id'],
-        'customer_id': order_row['customer_id'],
-        'created_at': str(order_row['created_at']),
-        'customer': {
-            'id': order_row['customer_id'],
-            'name': order_row['customer_name'],
-            'email': order_row['customer_email'],
-        }
-    }
 
     products_query = """
         SELECT 
@@ -87,7 +74,26 @@ async def get_order(order_id: int):
         WHERE 
             op.order_id IN (:order_id);
     """
-    products_rows = await database.fetch_all(query=products_query, values={"order_id": order_id})
+
+    order_row, products_rows = await asyncio.gather(
+        database.fetch_one(query=order_query, values={"order_id": order_id}),
+        database.fetch_all(query=products_query, values={"order_id": order_id})
+    )
+
+    if not order_row:
+        raise HTTPException(status_code=404, detail="Order not found")
+
+    order_instance = {
+        'id': order_row['id'],
+        'customer_id': order_row['customer_id'],
+        'created_at': str(order_row['created_at']),
+        'customer': {
+            'id': order_row['customer_id'],
+            'name': order_row['customer_name'],
+            'email': order_row['customer_email'],
+        }
+    }
+
 
     product_data = [
         {'id': row['id'], 'name': row['name'], 'price': row['price']} for row in products_rows
